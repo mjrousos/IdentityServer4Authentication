524f6d32616a7750bb1eb8f7158146c088dd46c4
Mike Rousos <MikeRou@Microsoft.com>
2016-11-11 11:57:13 -0800

    Remove unnecessary files and scripts
    Everything intersting in this sample happens on the backend

--- src/IdentityServer4Authentication/wwwroot/_references.js
+++ src/IdentityServer4Authentication/wwwroot/_references.js
@@ -1,5 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
